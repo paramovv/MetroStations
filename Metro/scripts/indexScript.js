c35cb582dfcbe51441aa8461451fc5592483cf6e
[Veronika Paramonova] <[paramovv@gmail.com]>
2021-05-20 00:39:26 -0400

    Final version

---
+++ Metro/scripts/indexScript.js
@@ -0,0 +1,9 @@
+/*clock*/
+var myVar = setInterval(function() {
+  myTimer();
+}, 1000);
+
+function myTimer() {
+  var d = new Date();
+  document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+}
